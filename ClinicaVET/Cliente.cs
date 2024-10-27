831089bb9630958ae6567088a00098214a8d9504
Nox06 <yedicxonatencio2006@gmail.com>
2024-10-27 11:00:12 -0400

    Nox06

---
+++ ClinicaVET/Cliente.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ClinicaVET
+{
+    class Cliente
+    {
+       /* private string nombre;
+        private string apellido;
+        private int edad;
+        private int ID;
+        public List<Servicio> historial;*/
+
+        [JsonPropertyName("nombre")]
+        public string nombre {  get; set; }
+
+        [JsonPropertyName("apellido")]
+        public string apellido { get; set; }
+
+        [JsonPropertyName("edad")]
+        public int edad { get; set; }
+
+        [JsonPropertyName("ID")]
+        public int ID { get; set; }
+
+        [JsonPropertyName("historial")]
+        public List<Servicio> historial { get; set; }
+
+        public Cliente(string n, string a, int e, int i)
+        {
+            this.nombre = n;
+            this.apellido = a;
+            this.edad = e;
+            this.ID = i;
+            this.historial = new List<Servicio>();
+        }
+
+
+        public string getNombre()
+        {
+            return this.nombre;
+        }
+
+        public int getID()
+        {
+            return ID;
+        }
+        public void agregar(Servicio A)
+        {
+            this.historial.Add(A);
+        }
+
+        public bool chequear(int c)
+        {
+            foreach (Servicio i in this.historial)
+            {
+                if (i.codigo == c)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool Tiene(String n)
+        {
+            foreach(Servicio aux in this.historial)
+            {
+                if (n == aux.codigo.ToString())
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ ClinicaVET/Gerente.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ClinicaVET
+{
+    static class Gerente
+    {
+        public static List<Cliente> ListaCliente = new List<Cliente>();
+        public static List<Mascotas> ListaMascota = new List<Mascotas>();
+        public static int op;
+
+        public static void guardar()
+        {
+            string path = @"C:\Users\yedic\Desktop\ClinicaVET\ClinicaVET\bin\Debug\listaCliente.json";
+
+            string json = JsonSerializer.Serialize(ListaCliente);
+            File.WriteAllText(path, json);
+        }
+
+        public static void Leer()
+        {
+            string path = @"C:\Users\yedic\Desktop\ClinicaVET\ClinicaVET\bin\Debug\listaCliente.json";
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                IncludeFields = true
+            };
+
+            string json = "[{\"nombre\":\"Juan\",\"apellido\":\"Pérez\",\"edad\":30,\"ID\":1}]";
+            List<Cliente> clientes1 = JsonSerializer.Deserialize<List<Cliente>>(json, options);
+
+
+        }
+
+        public static bool verificar(int ID)
+        {
+            foreach (Cliente aux in ListaCliente)
+            {
+                if (ID == aux.getID())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static void insertar(int i, int c, float m, string nm, string di, float pe, int dd, int me, int aa, string T)
+        {
+            foreach(Cliente aux in ListaCliente)
+            {
+                if (aux.getID() == i)
+                {
+                    if (aux.chequear(c) == false)
+                    {
+                        Servicio nuevoCliente = new Servicio();
+                        nuevoCliente.codigo = c;
+                        nuevoCliente.monto = (int)m;
+                        aux.agregar(nuevoCliente);
+                        Mascotas nuevo;
+
+                        if (T == "Perro")
+                        {
+                            nuevo = new Perro(nm, di, pe, c, new Fecha(dd, me, aa));
+                        }
+                        else
+                        {
+                            if(T == "Gato")
+                            {
+                                nuevo = new Gato(nm, di, pe, c, new Fecha(dd, me, aa));
+                            }
+                            else
+                            {
+                                nuevo = new Ave(nm, di, pe, c, new Fecha(dd, me, aa));
+                            }
+                        }
+                        ListaMascota.Add(nuevo);
+                    }
+                }
+            }
+        }
+
+        public static void insertar(string n, string a, int e, int i, int c, float m, string nm, string di, float pe, int dd, int me, int aa, string T)
+        {
+            Cliente C = new Cliente(n,a,e,i);
+            C.agregar(new Servicio(c,m));
+            ListaCliente.Add(C);
+            guardar();
+
+            Mascotas nuevo;
+
+            if (T == "Perro")
+            {
+                nuevo = new Perro(nm, di, pe, c, new Fecha(dd, me, aa));
+            }
+            else
+            {
+                if (T == "Gato")
+                {
+                    nuevo = new Gato(nm, di, pe, c, new Fecha(dd, me, aa));
+                }
+                else
+                {
+                    nuevo = new Ave(nm, di, pe, c, new Fecha(dd, me, aa));
+                }
+            }
+            ListaMascota.Add(nuevo);
+        }
+
+        public static void enviar(string n, string a, int e, int ID, int c, float m, string nm, string di, float pe, int dd, int me, int aa, string T)
+        {
+            if (verificar(ID) == true)
+            {
+                insertar(ID, c, m,nm,di,pe,dd,me,aa,T);
+            }
+            else
+            {
+                insertar(n,a,e,ID,c,m,nm, di, pe, dd, me, aa, T);
+            }
+        }
+    }
+}
